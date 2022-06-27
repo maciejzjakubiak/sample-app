4c9603e189afeba79988d88fe8f56a43eec28875
Maciej Jakubiak <maciej.z.jakubiak@gmail.com>
2022-06-27 12:47:57 -0500

    3 pages on local host

--- app/controllers/example_pages_controller.rb
+++ app/controllers/example_pages_controller.rb
@@ -1,2 +1,13 @@
 class ExamplePagesController < ApplicationController
+  def greeting
+    render json: { message: "Hello" }
+  end
+
+  def us
+    render html: { message: "This is us" }
+  end
+
+  def contact
+    render json: { message: "Do not reach out" }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get "/home", controller: "example_pages", action: "greeting"
+  get "/about us", controller: "example_pages", action: "us"
+  get "/contact us", controller: "example_pages", action: "contact"
 end
